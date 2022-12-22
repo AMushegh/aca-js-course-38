b13914cedeeb9dec67aa8428c0f186934d32e060
Mushegh Andrasyan <mushegh.andrasyan@gmail.com>
2022-12-22 21:28:15 +0400

    adds lessons

---
+++ pre-exam-lesson/hoisting.js
@@ -0,0 +1,16 @@
+// console.log(a);
+
+// var a = 10;
+
+// console.log(a);
+
+// b();
+
+// var b = function () {};
+
+// function foo(a) {
+//   var a = 10;
+//   console.log(a);
+// }
+
+// foo(097786987);
---
+++ pre-exam-lesson/index.js
@@ -0,0 +1,60 @@
+// const n = new Number(5);
+
+// console.log(n, 5);
+// console.log(typeof n);
+// console.log(typeof 5);
+
+// const str = "somestring";
+
+// console.log(str.split(""));
+
+// function foo() {}
+
+// console.log(typeof foo);
+
+// const falsyValues = [false, 0, -0, null, undefined, "", NaN];
+
+// for (let value of falsyValues) {
+//   console.log(Boolean(value));
+// }
+
+// console.log(Boolean(foo));
+
+// function foo() {
+//   10 > 4;
+// }
+
+// console.log(foo());
+
+// const foo1 = (a, b) => ({ a, b });
+
+// console.log(foo1("name", 567));
+
+// const name = "Mushegh";
+// const obj = {
+//   name,
+//   surname: "Andrasyan",
+// };
+
+// console.log(obj);
+
+// const a = {
+//   numbers: [1, 2, 3, 4],
+//   key: "value",
+
+//   showNumbers() {
+//     console.log(this);
+//     this.numbers.forEach((number) => {
+//       console.log(number, this.key);
+//     });
+//   },
+// };
+
+// a.showNumbers();
+// var numbers = [5, 6, 7, 8];
+// let b = a.showNumbers;
+// b();
+
+// function foo() {}
+
+// console.log(foo === a.showThis); // true
